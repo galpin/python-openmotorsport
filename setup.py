7eeb8bc7e5cbaf168f96b4f8ce1b39b6c846cb26
Martin Galpin <m@66laps.com>
2010-07-24 12:23:24 +0100

    Adds basic setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='python-openmotorsport',
+  version='1.0',
+  description='A library that provides a python interface the OpenMotorsport format.',
+  author='Martin Galpin',
+  author_email='m@66laps.com',
+  url='http://developer.66laps.com/',
+  packages=['openmotorsport']
+)
