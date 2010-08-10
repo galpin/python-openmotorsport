b1205c37ff205b68bc5c1a7a033dec445cd6642d
Martin Galpin <m@66laps.com>
2010-08-10 20:09:19 +0100

    Initial commit of new TimeSeries classes (and test cases) in openmotorsport.time

---
+++ openmotorsport/time.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+#
+# A library that provides a python interface the OpenMotorsport format.
+#
+# Author: Martin Galpin (m@66laps.com)
+#
+# Copyright 2007 66laps Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+from scipy import interpolate
+from scipy import signal
+
+# Base time is currently milliseconds (sufficient for up to 1KHz)
+BASE_TIME = 1000
+
+class Frequency(object):
+  '''
+  This class represents a sampling rate (and interval).
+  '''
+  def __init__(self, frequency):
+    '''
+    Creates a new instance of Frequency. Takes a sampling frequency:
+
+    >>> f = Frequency(5) # 5Hz
+    >>> f.frequency
+    5
+    >>> f.interval
+    200
+    
+    '''
+    self._frequency = frequency
+    self._interval = BASE_TIME / frequency
+
+  @staticmethod
+  def from_interval(interval):
+    '''
+    A convienience method to create a Frequency object from a sampling interval.
+    Takes a sampling interval in milliseconds:
+
+    >>> f = Frequency.from_interval(200)
+    >>> f.interval
+    200
+    >>> f.frequency
+    5
+    '''
+    f = Frequency(BASE_TIME / interval)
+    return f
+
+  @property
+  def interval(self):
+    return self._interval
+
+  @property
+  def frequency(self):
+    return self._frequency
+
+  def __repr__(self):
+    return '%dHz' % self._frequency
+
+  def __eq__(self, other):
+    return other and self.frequency == other.frequency
+
+
+class Epoch(object):
+  '''
+  This class represents a period of time of a given length and offset.
+  '''
+  def __init__(self, length, offset=0):
+    self._length = length
+    self._offset = offset
+
+  @property
+  def length(self):
+    return self._length
+
+  @property
+  def offset(self):
+    return self._offset
+
+class BaseTimeSeries(object):
+  '''
+  BaseTimeSeries is an abstract base class to all TimeSeries implementations.
+  This class should not be instantiated.
+  '''
+  def __init__(self):
+    self._offset = 0
+    self._data = np.array([], dtype=np.float32)
+
+  @property
+  def offset(self):
+    '''Gets the start time of this time series'''
+    return self._offset
+
+  @property
+  def data(self):
+    return self._data
+
+  @property
+  def duration(self):
+    raise NotImplementedError('This method is not implemented.')    
+
+  @property
+  def end_time(self):
+    '''Gets the end time of this time series (start time plus duration).'''
+    return self._offset + self.duration
+
+  def at(self, time):
+    '''Gets a data sample at a given time.'''
+    raise NotImplementedError('This method is not implemented.')
+
+  def get(self, index):
+    '''Gets a data sample at a given index.'''
+    return self._data[index]
+
+  def slice(self, epoch):
+    raise NotImplementedError('This method is not implemented.')
+
+  def resample(self, frequency):
+    raise NotImplementedError('This method is not implemented.')
+
+  def __len__(self):
+    return np.size(self._data)
+
+class VariableTimeSeries(BaseTimeSeries):
+  '''This class represents a time series with a variable sampling rate.'''
+
+  def __init__(self, data=[], times=[], offset=0):
+    '''
+    Create a new instance of VariableTimeSeries.
+
+    Raises ValueError if data and times are not of equal length.
+    '''
+    self._data = np.array(data, dtype=np.float32)
+    self._times = np.array(times, dtype=np.float32)
+    self._offset = offset
+      
+    if np.size(self.data) != np.size(self.times):
+      raise ValueError('Data/times mismatch. Lengths must be equal.')
+
+  @property
+  def times(self):
+    '''Gets the array of sample times.'''
+    return self._times
+
+  @property
+  def duration(self):
+    '''Gets the duration of this time series.'''
+    return 0 if not len(self._times) else self._times[-1]
+
+  def at(self, time):
+    '''Gets a data sample at a given time using linear interpolation.'''
+    f = interpolate.interp1d(self._times, self._data)  # TODO cache
+    return f(time)
+
+  def index_at(self, time):
+    indices = np.where(self.times >= time)[0]
+    if not np.size(indices):
+      raise ValueError('Time exceeds length of time series.')
+    return indices[0]
+
+  def slice(self, epoch):
+    '''
+    Gets a new instance of VariableTimeSeries that contains only the data/times
+    for a given epoch. This method currently does not implement interpolation
+    and will only return actual actual data samples.
+    '''
+    start = self.index_at(epoch.offset)
+    end = self.index_at(epoch.offset + epoch.length) + 1# inclusive
+    
+    return VariableTimeSeries(
+      data=self.data[start:end],
+      times=self.times[start:end],
+      offset=epoch.offset
+    )
+
+  def append(self, data, time):
+    '''
+    Appends a value and times to this time series.
+
+    Raises ValueError if value and time are not equal in length.    
+    '''
+    if np.size(data) != np.size(time):
+      raise ValueError('Data/times mismatch. Lengths must be equal.')
+
+    self._data = np.append(self._data,
+                           np.asanyarray(data, dtype=self._data.dtype))
+    self._times = np.append(self._times,
+                            np.asanyarray(time, dtype=self._data.dtype))
+
+
+class UniformTimeSeries(VariableTimeSeries):
+  '''
+  This class represents a time series with uniform data samples.
+  '''
+  def __init__(self, frequency, data=[], offset=0):
+    self._frequency = frequency
+    self._data = np.array(data, dtype=np.float32)
+    self._offset = offset
+
+  @property
+  def frequency(self):
+    return self._frequency
+
+  @property
+  def duration(self):
+    return len(self) * self._frequency.interval
+
+  def append(self, data):
+    '''Appends a given data sample to this time series.'''
+    self._data = np.append(self._data,
+                           np.asanyarray(data, dtype=self._data.dtype))
+
+  def at(self, time):
+    '''Gets a data sample at a given time using linear interpolation.'''    
+    times = np.arange(self.offset, self.end_time, self.frequency.interval)
+    f = interpolate.interp1d(times, self.data)
+    return f(time)
+
+  def slice(self, epoch):
+    times = np.arange(self.offset, self.end_time, self.frequency.interval)
+    f = interpolate.interp1d(times, self.data)
+    epoch_times = np.arange(epoch.offset, epoch.offset + epoch.length,
+                            self.frequency.interval)
+    
+    return UniformTimeSeries(
+      frequency=self.frequency,
+      data=f(epoch_times),
+      offset=epoch.offset
+    )
+
+  def resample(self, frequency):
+    if frequency == self.frequency or not np.size(self.data):
+      return self
+    elif frequency.frequency > self.frequency.frequency:
+      return self._upsample(frequency)
+    return self._downsample(frequency)
+
+  def _downsample(self, frequency):
+    factor = self.frequency.frequency / frequency.frequency
+    return signal.decimate(self.data, factor)
+
+  def _upsample(self, frequency):
+    factor = frequency.frequency / self.frequency.frequency
+    # TODO find appropriate resampling method
+    return signal.resample(self.data, factor * len(self))
+    
---
+++ tests/time_tests.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python
+#
+# A library that provides a python interface the OpenMotorsport format.
+#
+# Author: Martin Galpin (m@66laps.com)
+#
+# Copyright 2007 66laps Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from openmotorsport.time import *
+from numpy.testing.utils import assert_array_equal
+
+class FrequencyTests(unittest.TestCase):
+  def test_frequency(self):
+    f = Frequency(5) # 5Hz
+    self.assertEquals(f.frequency, 5)
+    self.assertEquals(f.interval, 200)
+    self.assertEquals(f.__repr__(), '5Hz')
+    self.assertEquals(f, Frequency.from_interval(200))
+
+class VariableTimeSeriesTests(unittest.TestCase):
+  def test_VariableTimeSeries(self):
+    ts = VariableTimeSeries()
+    self.assertEquals(len(ts), 0)
+    self.assertEquals(len(ts.times), 0)
+
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    self.assertEquals(len(ts), 3)
+    self.assertEquals(len(ts.times), 3)
+
+    ts = VariableTimeSeries(np.array([1,2,3], dtype=np.float32),
+                            np.array([1,2,3], dtype=np.float32))
+    self.assertEquals(len(ts), 3)
+    self.assertEquals(len(ts.times), 3)
+
+    ts = VariableTimeSeries(np.array([1,2,3], dtype=np.float64),
+                            np.array([1,2,3], dtype=np.float64))
+    self.assertEquals(len(ts), 3)
+    self.assertEquals(len(ts.times), 3)
+    self.assertEquals(ts.data.dtype, np.float32)
+    self.assertEquals(ts.times.dtype, np.float32)
+
+    self.assertRaises(ValueError, VariableTimeSeries, [1,2,3], [])
+    self.assertRaises(ValueError, VariableTimeSeries, [], [1,2,3])
+    self.assertRaises(ValueError, VariableTimeSeries, [1,2,3])
+
+  def test_index_at(self):
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    self.assertEquals(ts.index_at(1), 0)
+    self.assertEquals(ts.index_at(2), 1)
+    self.assertEquals(ts.index_at(3), 2)
+    self.assertEquals(ts.index_at(1.5), 1)
+    self.assertRaises(ValueError, ts.index_at, 3.5)
+
+  def test_append(self):
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    self.assertEquals(len(ts), 3)
+    self.assertEquals(len(ts.times), 3)
+
+    ts.append([4,5,6], [4,5,6])
+    self.assertEquals(len(ts), 6)
+    self.assertEquals(len(ts.times), len(ts))
+
+    ts.append(7,7)
+    self.assertEquals(len(ts), 7)
+    self.assertEquals(len(ts.times), len(ts))
+
+    ts.append(np.array([8,9,10], dtype=np.float32),
+              np.array([8,9,10], dtype=np.float32))
+    self.assertEquals(len(ts), 10)
+    self.assertEquals(len(ts.times), len(ts))
+
+    self.assertRaises(ValueError, ts.append, [1,2,3], [])
+
+  def test_at(self):
+    ts = VariableTimeSeries()
+    self.assertRaises(ValueError, ts.at, 1)
+
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    self.assertRaises(ValueError, ts.at, 0)
+    self.assertEquals(ts.at(2), 2)
+    self.assertEquals(ts.at(3), 3)
+    self.assertEquals(ts.at(2.5), 2.5)
+    self.assertRaises(ValueError, ts.at, 4)    
+
+  def test_duration(self):
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    self.assertEquals(ts.duration, 3)
+
+    ts = VariableTimeSeries()
+    self.assertEquals(ts.duration, 0)
+    self.assertEquals(ts.end_time, 0)
+
+    ts = VariableTimeSeries([1,2,3], [1,2,3], offset=4)
+    self.assertEquals(ts.end_time, 7)
+
+  def test_slice(self):
+    ts = VariableTimeSeries([1,2,3], [1,2,3])
+    assert_array_equal(
+      ts.slice(Epoch(offset=ts.offset, length=ts.duration)).data,
+      [1,2,3]
+    )
+    assert_array_equal(ts.slice(Epoch(offset=1, length=2)).data, [1,2,3])
+    self.assertRaises(ValueError, ts.slice, Epoch(offset=0, length=4))
+
+class TimeSeriesTests(unittest.TestCase):
+  def test_TimeSeries(self):
+    ts = UniformTimeSeries(Frequency(5))
+    self.assertEquals(ts.frequency, Frequency(5))
+    self.assertEquals(len(ts), 0)
+
+    ts = UniformTimeSeries(Frequency(5), [1,2,3])
+    self.assertEquals(len(ts), 3)
+
+    ts = UniformTimeSeries(Frequency(5), np.array([1,2,3], dtype=np.float32))
+    self.assertEquals(len(ts), 3)
+    self.assertEquals(ts.data.dtype, np.float32)
+
+    ts = UniformTimeSeries(Frequency(5), [1,2,3,4,5])
+    self.assertEquals(ts.offset, 0)
+    self.assertEquals(ts.duration, 1000)
+
+  def test_append(self):
+    ts = UniformTimeSeries(Frequency(5), [1,2,3])
+    self.assertEquals(len(ts), 3)
+    ts.append(4)
+    self.assertEquals(len(ts), 4)
+    ts.append([5,6,7])
+    self.assertEquals(len(ts), 7)
+    ts.append(np.array([8,9,10], dtype=np.float32))
+    self.assertEquals(len(ts), 10)
+    self.assertEquals(ts.duration, 2000)
+    self.assertEquals(ts.end_time, 2000)
+
+    ts = UniformTimeSeries(Frequency(5), [1,2,3], offset=1000)
+    ts.append([5,6,7])
+    self.assertEquals(ts.duration, 1200)
+    self.assertEquals(ts.end_time, 2200)
+
+  def test_at(self):
+    ts = UniformTimeSeries(Frequency(5), [10,20,30,40,50,60,70])
+    self.assertEquals(ts.duration, 1400)
+    self.assertEquals(ts.at(0), 10)
+    self.assertEquals(ts.at(200), 20)
+    self.assertEquals(ts.at(1200), 70)
+    self.assertRaises(ValueError, ts.at, 1400)
+
+  def test_slice(self):
+    ts = UniformTimeSeries(Frequency(5), [10,20,30,40,50,60,70])
+    assert_array_equal(ts.slice(Epoch(offset=0, length=600)).data, [10,20,30])
+    assert_array_equal(ts.slice(Epoch(length=ts.end_time)).data, ts.data)
+    assert_array_equal(ts.slice(Epoch(length=ts.end_time)).data, ts.data)
+    self.assertRaises(ValueError, ts.slice, Epoch(offset=0, length=1600))
+
+  def test_downsample(self):
+    ts = UniformTimeSeries(Frequency(10), [10,20,30,40,50,60,70,80,90,100])
+    self.assertEqual(len(ts.resample(Frequency(5))), 5)
+    ts = UniformTimeSeries(Frequency(5), [10,20,30,40,50,60,70,80,90,100])
+    self.assertEqual(len(ts.resample(Frequency(1))), 2)
+    ts = UniformTimeSeries(Frequency(5), [])
+    self.assertEqual(len(ts.resample(Frequency(1))), 0)    
+
+  def test_upsample(self):
+    ts = UniformTimeSeries(Frequency(5), [10,20,30,40,50,60,70,80,90,100])
+    self.assertEqual(len(ts.resample(Frequency(10))), 20)
+    ts = UniformTimeSeries(Frequency(5), [10,20,30,40,50,60,70,80,90,100])
+    self.assertEqual(len(ts.resample(Frequency(20))), 40)
+    ts = UniformTimeSeries(Frequency(5), [])
+    self.assertEqual(len(ts.resample(Frequency(10))), 0)
+
+if __name__ == '__main__':
+  unittest.main()
